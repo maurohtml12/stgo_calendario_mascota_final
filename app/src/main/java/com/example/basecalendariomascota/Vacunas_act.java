0d3de6d9f9e242d187669d5fac8bd408b2655d34
maurohtml12 <mauricio.sobarzo.h@gmail.com>
2021-11-13 03:30:35 -0300

    commit 0.3 // vaccines activity is completed // database for vaccines is added // add, delete, modify, consult is implemented

--- app/src/main/java/com/example/basecalendariomascota/Vacunas_act.java
+++ app/src/main/java/com/example/basecalendariomascota/Vacunas_act.java
@@ -2,13 +2,149 @@ package com.example.basecalendariomascota;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.basecalendariomascota.database.AdminSQLiteOpenHelper;
 
 public class Vacunas_act extends AppCompatActivity {
+    private EditText numero, vaccin, inoc, next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacunas);
+        numero = findViewById(R.id.number);
+        vaccin = findViewById(R.id.vaccine);
+        inoc = findViewById(R.id.dateinoc);
+        next = findViewById(R.id.datenext);
+    }
+
+    // metodos para guardar vacunas
+
+    public void saveVacuna(View view){
+        //obtengo mi base de datos
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "calendariomascota", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();//permisos de sobre escritura...
+        //obtengo los datos de la interfaz
+        String num = numero.getText().toString();
+        String vac = vaccin.getText().toString();
+        String ino = inoc.getText().toString();
+        String nex = next.getText().toString();
+
+        if (num.isEmpty() || vac.isEmpty() || !ino.isEmpty() || nex.isEmpty()){
+            //creo el registro
+            ContentValues conten = new ContentValues();//contiene valores
+            conten.put("numero", num);
+            conten.put("vacuna", vac);
+            conten.put("inoculacion", ino);
+            conten.put("proxima", nex);
+            //insertar el registro
+            bd.insert("vacunas", null, conten);
+            bd.close();
+            cleanVacuna(view);
+            Toast.makeText(this, "Registro guardado", Toast.LENGTH_SHORT).show();
+
+        }else{
+            //mensaje de error
+            Toast.makeText(this, "No se puede guardar vacuna sin datos", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+
+    // metodos para consultar vacunas
+
+    public void consultVacuna(View view){
+        //obtengo mi base de datos
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "calendariomascota", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();//permisos de sobre escritura...
+
+        String codigo = numero.getText().toString();
+
+        if(!codigo.isEmpty()){
+            Cursor file = bd.rawQuery("select * from vacunas where numero="+codigo, null);
+            if(file.moveToFirst()){
+                vaccin.setText(file.getString(1));
+                inoc.setText(file.getString(2));
+                next.setText(file.getString(3));
+
+            }else{
+                Toast.makeText(this, "No existe vacuna con ese numero", Toast.LENGTH_SHORT).show();
+            }
+
+        }else{
+            Toast.makeText(this, "No se puede consultar vacuna sin datos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    //metodo para eliminar vacunas
+
+    public void deleteVacuna(View view){
+        //obtengo mi base de datos
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "calendariomascota", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();//permisos de sobre escritura...
+
+        String codigo = numero.getText().toString();
+
+        if(!codigo.isEmpty()){
+            int cant = bd.delete("vacunas", "numero="+codigo, null);
+            if(cant == 1){
+                bd.close();
+                cleanVacuna(view);
+                Toast.makeText(this, "Registro eliminado", Toast.LENGTH_SHORT).show();
+
+            }
+        }else{
+            Toast.makeText(this, "No se puede eliminar vacuna sin datos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    //metodo para actualizar vacunas
+
+    public void updateVacuna(View view){
+        //obtengo mi base de datos
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "calendariomascota", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();//permisos de sobre escritura...
+        //obtengo los datos de la interfaz
+        String num = numero.getText().toString();
+        String vac = vaccin.getText().toString();
+        String ino = inoc.getText().toString();
+        String nex = next.getText().toString();
+
+        if (num.isEmpty() || vac.isEmpty() || !ino.isEmpty() || nex.isEmpty()){
+            //creo el registro
+            ContentValues conten = new ContentValues();//contiene valores
+            conten.put("numero", num);
+            conten.put("vacuna", vac);
+            conten.put("inoculacion", ino);
+            conten.put("proxima", nex);
+            //insertar el registro
+            bd.update("vacunas", conten, "numero="+num, null);
+            bd.close();
+            cleanVacuna(view);
+            Toast.makeText(this, "Registro actualizado", Toast.LENGTH_SHORT).show();
+
+        }else{
+            //mensaje de error
+            Toast.makeText(this, "No se puede guardar vacuna sin datos", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+    }
+    //metodo para limpiar campos
+
+    public void cleanVacuna(View view){
+        numero.setText("");
+        vaccin.setText("");
+        inoc.setText("");
+        next.setText("");
     }
 }
---
+++ app/src/main/java/com/example/basecalendariomascota/database/AdminSQLiteOpenHelper.java
@@ -0,0 +1,28 @@
+package com.example.basecalendariomascota.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    //contructor para instancia base de datos...
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+     //para definir modelo de trabajo...
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+         //se define modelo de trabajo...
+        db.execSQL("CREATE TABLE vacunas(numero int primary key, vacuna text, inoculacion text, proxima text)");
+
+    }
+     //permite realizar cambio esquematicos
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
